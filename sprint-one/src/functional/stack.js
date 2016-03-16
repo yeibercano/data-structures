bedd1f3955591ef70d90d67fea520a196f0e48f0
yeibercano <yeibercano@gmail.com>
2016-03-16 11:19:06 -0700

    size 2 test pass

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,21 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // add a count variable
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    //increment count - everytime we call someInstance.push, count will increase by one
+    count++;
   };
 
   someInstance.pop = function(){
   };
 
   someInstance.size = function(){
-    return 0;
+    //return count
+    return count;
   };
 
   return someInstance;
