4f871d6165456f750f41060eab50fec4b8896170
yeibercano <yeibercano@gmail.com>
2016-03-16 15:36:40 -0700

    stack shared behavior complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,19 +5,14 @@ var Stack = function(){
   var storage = {};
   // add a count variable
   var count = 0;
-  //create variable to store keys an empty object
-
-  var allKeys = {};
-
-  // acces the last key added
-  var last = Object.keys(allKeys)[Object.keys(allKeys).length - 1];
-
-
+  //create variable to hold the last item in the object
+  var last;
   // Implement the methods below
   someInstance.push = function(value){
-    //add keys to allKeys obj
-    allKeys[value] = value;
-    console.log('allKeys inside push:', allKeys)
+    //add keys to storage obj
+    storage[value] = value;
+    //sets last variable to last added to storage object
+    last = Object.keys(storage)[Object.keys(storage).length - 1];
     //increment count - everytime we call someInstance.push, count will increase by one
     count++;
   };
@@ -25,20 +20,18 @@ var Stack = function(){
   someInstance.pop = function(){
     //decrease count by 1 each time someInstance.pop is called
     count--;
-    //remove last item 
-    delete allKeys.a;
-    //return last item
-    console.log('all Kesy inside pop:', allKeys)
+    //return last
+    return last;
   };
 
   someInstance.size = function(){
-    //if count is 0 return 0 otherwise return count
+    //if count is 0 or less, return 0, otherwise return count
     if (count <= 0) {
       return 0;
     } else {
-      return count
+      return count;
     };
   }
-
+  //return someInstance
   return someInstance;
 };
