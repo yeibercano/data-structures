575306d27690dbb55f3a828bb3d484ccdb197a01
yeibercano <yeibercano@gmail.com>
2016-03-16 17:31:54 -0700

    completes queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,17 +4,37 @@ var Queue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  // create a count variable
+  var count = 0;
+  // create a variable last to hold last item in storage
+  var first;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    //increase count by one
+    count++;
+    //add item into storage
+    storage[value] = value;
   };
 
   someInstance.dequeue = function(){
+    //decrease count by 1
+    count--;
+    //assign the last item to the last variable in storage
+    first = Object.keys(storage)[0];
+    //remove last item in storage
+    delete storage[Object.keys(storage)[0]];
+    console.log('storage:', storage)
+    return first;
   };
 
   someInstance.size = function(){
-    //returns 0
-    return 0;
+    //check if count is equal or less than 0
+    if (count <= 0) {
+      return 0;
+    } else {
+      return count;
+    }
   };
 
   return someInstance;
