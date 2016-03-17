f2192b66c064bfc4d08e467b4c339c84c5974d15
yeibercano <yeibercano@gmail.com>
2016-03-17 16:46:32 -0700

    prototype stack return 0 test pass

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //create a new object 
+  var newStack = Object.create(stackMethods);
+
+
+
+  //return newStack
+  return newStack;
 };
 
 var stackMethods = {};
 
+//create size method
+stackMethods.size = function() {
+  //return 0
+  return 0;
+};
 
