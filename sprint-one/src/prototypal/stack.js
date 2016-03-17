8fc92a9952ea5f31a49bbc106110d5645c5e8187
yeibercano <yeibercano@gmail.com>
2016-03-17 16:56:11 -0700

    prototypal pattern pop test pass

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -5,17 +5,32 @@ var Stack = function() {
   //create a new object 
   var newStack = Object.create(stackMethods);
 
-
+  newStack.counter = 0   
 
   //return newStack
   return newStack;
 };
 
 var stackMethods = {};
+//create push method
+stackMethods.push = function(value) {
+  //increment counter by 1 
+  this.counter++;
+  //assign a value to a key value pair  
+  this[value] = value;
+};  
+//create a pop method
+stackMethods.pop = function() {
+
+};
 
 //create size method
 stackMethods.size = function() {
   //return 0
-  return 0;
+  if(this.counter === 0) {
+    return 0;
+  } else {
+    return this.counter; 
+  }
 };
 
