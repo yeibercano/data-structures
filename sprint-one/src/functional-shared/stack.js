4986c817e85790ce69d6866a6f86378ba4b43f01
yeibercano <yeibercano@gmail.com>
2016-03-17 10:48:46 -0700

    returns 0, passes

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // create empty obj
+  var obj = {};
+  //add new methods to obj 
+  _.extend(obj, stackMethods);
+
+  //return obj
+  return obj
 };
 
 var stackMethods = {};
+//add methods to stackMethods
+stackMethods.size = function() {
 
+  return 0;
+
+};
 
