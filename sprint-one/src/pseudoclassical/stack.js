9585ae24ba2b31039bc9b4113383233b4a2c6633
Aralya Phinith <aphinith84@gmail.com>
2016-03-18 00:00:58 -0700

    pseudoclassical stack size method returns correct counter value

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -14,11 +14,16 @@ Stack.prototype.push = function() {
 
 //create a pop method
 Stack.prototype.pop = function() {
-  
+  // decrement counter by one whenever pop method is invoked
+  this.counter--;
 }
 
 //create a size method
 Stack.prototype.size = function() {
-  //return this.counter
-  return this.counter;
+  //checks to see if this.counter is less than or equal to 0, if it is, return 0, else return this.counter
+  if (this.counter <= 0) {
+    return 0;
+  } else {
+    return this.counter;
+  }
 };
