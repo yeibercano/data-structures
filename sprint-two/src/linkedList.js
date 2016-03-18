e1ea7c5e05d2a4243706e0e931a42f835a9cf8ca
yeibercano <yeibercano@gmail.com>
2016-03-18 15:46:05 -0700

    adds new node to list obj

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,33 +3,21 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
   //create a counter
-  list.counter = 0;
-  // console.log('list:', list);
-  console.log('list:', list);
-    _.extend(list, Node());
-   // add a key: list.node set it equal to Node()
-   list.node = Node;
-   // invoke node() in addToTail and pass the value from add to tail
-   //console.log('list.node:', list.node(1));
+  //list.counter = 0;
+  console.log('this is list before:', list);
 
-  list.addToTail = function(value){
-    // _.extend(list, Node(value));
-    //invoke list.node with value
-    console.log('invoked in addToTail:', list.node(value))
-    //increment counter by 1 each time addToTail is invoked
-    list.counter++;
-    //add value to list
-    // console.log('counter:', list.counter)
-    list[list.counter] = value;
-    //console.log('list:', list)
+list.addToTail = function(value){
+
+    //invoke Node and assign it to list.tail, this creates the node simultaneously and stores its value in tail
+    list.tail = Node(value);
+    //add the node object into the list
+    list[value] = list.tail;
     //assign list.tail to equal the added item
-    list.tail = list.node(value);
-    // console.log('list.tail:', list.tail)
-    // console.log('tail:', this.tail);
+    console.log('this is in list after addToTail:', list);
     return list.tail;
-  };
+};
 
-  list.removeHead = function(){
+list.removeHead = function(){
   };
 
   list.contains = function(target){
@@ -46,8 +34,6 @@ var Node = function(value){
 
   return node;
 };
-//console.log(Node(1));
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
