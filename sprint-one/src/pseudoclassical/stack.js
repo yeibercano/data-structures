7a7be2df3df4a5259b5be3150ab633dc2bee4420
Aralya Phinith <aphinith84@gmail.com>
2016-03-17 23:49:26 -0700

    pseudoclassical stack reports size 2 test complete

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,9 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create a counter property to keep track of number of items pushed and pulled
+  this.counter = 0;
 };
+
+// create a push method
+Stack.prototype.push = function() {
+  // increase counter incremently whenever push method is invoked
+  this.counter++;
+}
 
 //create a size method
 Stack.prototype.size = function() {
-  return 0;
+  //return this.counter
+  return this.counter;
 }
