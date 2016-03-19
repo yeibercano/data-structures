bb7e794298718c8ed4f63f76f94ea76363af37f5
yeibercano <yeibercano@gmail.com>
2016-03-18 19:44:23 -0700

    adds children to the tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,26 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
+  // create property named value
+  // use underscore to extend newTree with treeMethods
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+// console.log(newTree)
   return newTree;
 };
 
-
   // your code here
-  newTree.children = null;  // fix me
-
+  // newTree.children = null;  // fix me
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  // newTree.children = null;  // fix me
+ // push value into newTree.children
+ this.children.push({value:value});
+ // reassing newTree.value to this.children[0];
+  // this.value = this.children[0];
 };
 
 treeMethods.contains = function(target){
@@ -26,4 +30,5 @@ treeMethods.contains = function(target){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ // Logarithm
  */
