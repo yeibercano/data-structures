ac4a6c6863272d7af1b5b879809ab52e6f299063
Yeiber Cano <yeibercano@gmail.com>
2016-03-19 23:01:04 -0700

    completes set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set.storage = {};
+  // console.log(set);
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+    this.storage[item] = item;
+    console.log(this.storage)
+  
 };
 
 setPrototype.contains = function(item){
+   return _.contains(this.storage, item)
 };
 
 setPrototype.remove = function(item){
+    delete this.storage[item];
 };
 
 /*
